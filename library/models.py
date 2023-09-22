310b9147cfe889a6f2f07f74c162578a2ba2dd27
konstantin <kkononenko3@gmail.com>
2023-09-22 09:37:34 +0300

    created book category model

--- library/models.py
+++ library/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from borrowing.models import Borrowing
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ["name"]
+        indexes = [
+            models.Index(fields=["name"]),
+        ]
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
