be310c30442901d93d21acc69ef72205adf7afe1
konstantin <kkononenko3@gmail.com>
2023-09-22 18:32:26 +0300

    created urls file endpoint for borrowing

---
+++ borrowing/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from borrowing.views import BorrowingViewSet
+
+router = routers.DefaultRouter()
+router.register("", BorrowingViewSet)
+
+urlpatterns = router.urls
+
+app_name = "borrowing"
