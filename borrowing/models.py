caea1a33a070db6b885c15cd6862cbb15e354fc3
konstantin <kkononenko3@gmail.com>
2023-09-22 18:22:28 +0300

    created borrowing model

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Borrowing(models.Model):
+    borrow_date = models.DateTimeField(auto_now_add=True)
+    expected_return = models.DateTimeField()
+    actual_return = models.DateTimeField(blank=True, null=True)
+    book = models.ForeignKey(
+        "library.Book",
+        on_delete=models.CASCADE,
+        related_name="borrowings",
+    )
+    user = models.ForeignKey(
+        "user.User", on_delete=models.CASCADE, related_name="borrowings"
+    )
+
+    @property
+    def is_active(self):
+        return self.actual_return is None
+
+    class Meta:
+        ordering = ["expected_return"]
+        verbose_name = "borrowing"
+        verbose_name_plural = "borrowings"
+
+    def __str__(self) -> str:
+        return str(self.borrow_date)
