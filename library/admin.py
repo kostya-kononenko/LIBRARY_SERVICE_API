749f4a66ecd1e303623a4d8e0a4c1cb8407e301e
konstantin <kkononenko3@gmail.com>
2023-09-22 09:42:58 +0300

    created admin functionality for Category, Book, Rating

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Category, Book, Rating, RatingStarBook
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+    ]
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "author",
+        "daily_fee",
+        "created",
+        "updated",
+    ]
+    list_filter = ["created", "updated"]
+    list_editable = ["daily_fee"]
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ("id", "star", "book")
+
+
+admin.site.register(RatingStarBook)
