d8d31e132889c206740cbc1b429dd32c1aac4b65
konstantin <kkononenko3@gmail.com>
2023-09-22 18:29:37 +0300

    created admin functionality for borrowing

--- borrowing/admin.py
+++ borrowing/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from borrowing.models import Borrowing
+
+
+@admin.register(Borrowing)
+class BorrowingAdmin(admin.ModelAdmin):
+    list_display = (
+        "book",
+        "user",
+        "borrow_date",
+        "expected_return",
+        "actual_return")
+    list_display_links = ("book",)
