d7d99bbd136b8cdabd7b697f95d8963a7347863c
konstantin <kkononenko3@gmail.com>
2023-09-22 09:40:38 +0300

    created BookSerializer, BookListSerializer, BookDetailSerializer

--- library/serializers.py
+++ library/serializers.py
@@ -11,3 +11,69 @@ class CategorySerializer(serializers.ModelSerializer):
             "name",
         )
 
+
+class BookSerializer(serializers.ModelSerializer):
+    created = serializers.DateTimeField(format="%d-%m-%Y %H:%M")
+    updated = serializers.DateTimeField(format="%d-%m-%Y %H:%M")
+
+    class Meta:
+        model = Book
+        fields = (
+            "id",
+            "category",
+            "title",
+            "author",
+            "image",
+            "description",
+            "daily_fee",
+            "cover",
+            "created",
+            "updated",
+            "is_available",
+        )
+
+
+class BookListSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field="name", read_only=True)
+    middle_star = serializers.IntegerField()
+    rating_user = serializers.BooleanField()
+
+    class Meta:
+        model = Book
+        fields = (
+            "id",
+            "category",
+            "title",
+            "author",
+            "image",
+            "description",
+            "daily_fee",
+            "cover",
+            "created",
+            "updated",
+            "category",
+            "middle_star",
+            "rating_user",
+            "is_available",
+        )
+
+
+class BookDetailSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field="name", read_only=True)
+
+    class Meta:
+        model = Book
+        fields = (
+            "id",
+            "category",
+            "title",
+            "author",
+            "image",
+            "description",
+            "daily_fee",
+            "cover",
+            "created",
+            "updated",
+            "category",
+            "is_available",
+        )
