be4829f18a5d7abf2ef6d9d8fc280bd2ae42858a
konstantin <kkononenko3@gmail.com>
2023-09-22 18:36:38 +0300

    created logic for count borrowing (use in payment system)

---
+++ borrowing/count_borrowing.py
@@ -0,0 +1,16 @@
+def count_total_price_start_borrowing(borrowing):
+    days_borrowing = (borrowing.expected_return - borrowing.borrow_date).days
+    if days_borrowing == 0:
+        price_in_cents = int(borrowing.book.daily_fee * 50)
+    else:
+        price_in_cents = int(days_borrowing * borrowing.book.daily_fee * 100)
+    return price_in_cents
+
+
+def count_total_price_end_borrowing(borrowing):
+    days_borrowing = (borrowing.actual_return - borrowing.expected_return).days
+    if days_borrowing <= 0:
+        price_in_cents = 0
+    else:
+        price_in_cents = int(days_borrowing * borrowing.book.daily_fee * 200)
+    return price_in_cents
