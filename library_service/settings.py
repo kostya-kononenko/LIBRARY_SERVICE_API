60579a651608d1c7f10dfe16c12af74eaf97e729
konstantin <kkononenko3@gmail.com>
2023-09-25 09:43:08 +0300

    added dockerfile, docker-compose file, docker ignore file and created custom management command wait db

--- library_service/settings.py
+++ library_service/settings.py
@@ -91,10 +91,10 @@ WSGI_APPLICATION = "library_service.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.getenv("DB_HOST"),
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
     }
 }
 
